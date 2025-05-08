8d5523031d14534230456e9060e48e5093caa4bb
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-08 12:43:31 +0900

    2025-05-08

---
+++ src/chap02/code2_1.kt
@@ -0,0 +1,19 @@
+package chap02
+
+//これはドックコメントです。
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ * ※このファイルの説明です
+ */
+
+//これはコメントです。
+//何でも好きなことを書いてください。
+
+//これは複数行対応のコメントです。
+/*We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.*/
+
+fun main(args: Array<String>) {
+    print("Hello, world!") //画面の文字を表示しています。
+}
---
+++ src/chap02/code2_4.kt
@@ -0,0 +1,11 @@
+package chap02
+
+/**
+ * ctrl+dでカーソルがある行が複製できる
+ */
+fun main() {
+    println("こんにちは")
+    println("はじめまして")
+    println("よろしく！")
+}
+
