bbc12c074f4bed2b540bfdc59e341927a38293be
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-13 10:35:23 +0900

    変数の宣言の仕方

---
+++ src/chap03/code3_2.kt
@@ -0,0 +1,13 @@
+package chap03
+//変数の宣言の仕方
+
+//val = 定数　var = 変数
+fun main(){
+    val message : String = "Hello, world!"
+    println(message)
+
+    /*型推論
+    val message = "Hello, world!"
+    これでもいいが、Javaのように何でも入れられるわけではない
+     */
+}
