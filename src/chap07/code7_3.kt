4ebeabd6820b8e40500086b257edc4eb90faa97c
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-13 12:38:52 +0900

    論理演算(&&_2)

---
+++ src/chap07/code7_3.kt
@@ -0,0 +1,11 @@
+package chap07
+//論理演算(&&_2)
+fun main() {
+    val delicious = true
+    val cheap = false
+    println("このレストランは美味しい:${delicious}")
+    println("このレストランは安くない:${cheap}")
+
+    val letsEat = delicious && cheap
+    println("食事する？:${letsEat}")
+}
