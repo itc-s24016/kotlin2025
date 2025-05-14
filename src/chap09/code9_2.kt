e8960e4dd88b172a5b8ee0dd182bbc4b963a8edc
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-14 12:39:52 +0900

    varの使い方

---
+++ src/chap09/code9_2.kt
@@ -0,0 +1,16 @@
+package chap09
+//varの使い方
+/***
+ * val:定数 var:変数
+ */
+fun main() {
+    var a = 100
+    var b = 10_000
+    var c = 1_000_000_000
+    var d = 1_000_000_000_000_000_000L
+    var e = 0.123f
+    var f = 0.123456789
+    var g = "こんにちは"
+
+
+}
