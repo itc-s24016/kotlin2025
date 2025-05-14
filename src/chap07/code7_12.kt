600b5940e5b4a9595ec9e9b71dfe5fd45f376229
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-14 10:25:51 +0900

    論理演算(not)

---
+++ src/chap07/code7_12.kt
@@ -0,0 +1,11 @@
+package chap07
+//論理演算(!_not)
+fun main() {
+    val a = true
+    val b = !a //not a = true じゃなく false に反転する
+    val c = !b //not b = false じゃなく true に反転する
+
+    println("a の値は ${a} です")
+    println("b の値は ${b} です")
+    println("c の値は ${c} です")
+}
