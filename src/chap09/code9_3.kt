4413d59c08df35a07dea323f7ad5626b77f6071b
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-14 12:40:07 +0900

    valとvarの違い

---
+++ src/chap09/code9_3.kt
@@ -0,0 +1,19 @@
+package chap09
+//valとvarの違い
+/***
+ * val:定数 var:変数
+ */
+fun main() {
+    var a = 100
+    println("a: ${a}")
+
+    a = 101
+    println("a: ${a}")
+
+    /*
+    もし val で実行すると...
+    val b = 100
+    b = 101
+    結果：val cannot be reassigned.
+     */
+}
