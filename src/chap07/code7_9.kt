98a1b872e8d768c2ea3fdd70e7229f783c12581e
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-14 09:51:40 +0900

    論理演算(||_1)

---
+++ src/chap07/code7_9.kt
@@ -0,0 +1,11 @@
+package chap07
+//論理演算(||_4)
+fun main() {
+    val delicious = false
+    val cheap = false
+    println("このレストランは美味しくない:${delicious}")
+    println("このレストランは安くない:${cheap}")
+
+    val letsEat = delicious || cheap
+    println("食事する？:${letsEat}")
+}
