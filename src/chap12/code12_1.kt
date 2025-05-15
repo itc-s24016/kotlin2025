8bb01996485e1298f69717cfbb05b38516ba2329
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-15 12:04:43 +0900

    when文

--- src/chap12/code12_1.kt
+++ src/chap12/code12_1.kt
@@ -4,6 +4,7 @@ fun main() {
     val x = "Hello"
 
     //Java における swhich文と同じ
+    //※一つマッチするものがあると他は実行せずに終了するので break が存在しない
     when (x) {
         "GoodMorning" -> println("おはようございます")
         "Hello" -> println("こんにちは")
