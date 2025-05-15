81b183027f8a0bef2874c9b54878ebc1455aaf52
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-15 11:11:16 +0900

    valで結果の上書きを防ぐ

--- src/chap11/code11_10.kt
+++ src/chap11/code11_10.kt
@@ -1,13 +1,30 @@
 package chap11
-
+//valで結果の上書きを防ぐ
 fun main() {
-    val age = 14
-    var message = ""
+    //-------------------------------
+    val age1 = 14
+    var message1 = ""
 
-    if (age in 13..15){
-        message = "中学生です"
+    if (age1 in 13..15){
+        message1 = "中学生です"
     }else{
-        message = "中学生ではありません"
+        message1 = "中学生ではありません"
     }
-    println(message)
+    //message = "小学生"
+    println(message1)
+    //-------------------------------
+    /*
+    この書き方だと、もし println する前に誤って message に他の値が入ってしまった場合、
+    結果が上書き出来てしまうので、以下の記述方法で防ぐ
+    */
+    val age2 = 14
+    val message2: String
+
+    if (age2 in 13..15){
+        message2 = "中学生です"
+    }else{
+        message2 = "中学生ではありません"
+    }
+    //message2 = '小学生です'　　//エラーになる
+    println(message2)
 }
