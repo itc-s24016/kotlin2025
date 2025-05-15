bc8614cd61f4df2b1fcf92af097b2c7adc716ca9
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-15 12:32:17 +0900

    a

--- src/chap13/code13_7.kt
+++ src/chap13/code13_7.kt
@@ -1,10 +1,10 @@
 package chap13
-//for文(downToとstep)
+//for文(downTo)
 fun main() {
-    //9から1へ2ずつ減らす
+    //1ずつ減らしたいときはdownTo を使う
 
     val x = 5
-    for (i in 9 downTo 1 step 2){
+    for (i in 9 downTo 1){
         println("${x} × ${i} は ${x * i} です")
     }
 }
