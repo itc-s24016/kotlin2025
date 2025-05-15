ba838eea7ca9bb3db5fb5ea38a992e42ba95c824
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-15 12:00:49 +0900

    whenの演習問題

---
+++ src/chap12/code12_6.kt
@@ -0,0 +1,23 @@
+package chap12
+//whenの演習問題
+import kotlin.random.Random
+/*
+code11_13 のプログラムを when で再実装してください
+*/
+fun main() {
+    val score = Random.nextInt(0, 100)
+
+    val borderC = 60
+    val borderB = 70
+    val borderA = 80
+    val borderS = 90
+
+    val judge = when (score) {
+        in 0..borderC -> "D"
+        in borderC..borderB -> "C"
+        in borderB..borderA -> "B"
+        in borderA..borderS -> "A"
+        else -> "S"
+    }
+    println("score ${score}: ${judge}")
+}
