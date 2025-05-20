6003630239e8868aa5c2caef2d134efca166977c
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-20 11:50:21 +0900

    リスト(MutableList) リストに追加

---
+++ src/chap17/code17_7.kt
@@ -0,0 +1,22 @@
+package chap17
+//リスト(MutableList)
+fun main() {
+    val a: MutableList<String> = mutableListOf("こんにちは", "Kotlin", "よろしく")
+    for ((i, x) in a.withIndex()) {
+        println("${i} : ${x}")
+    }
+    println("------------")
+
+    //インデックスを指定して追加していく
+    a.add(2, "どうぞ")
+
+    //リストの末尾に追加していく
+    a.add("お願いします")
+
+    //同様に末尾に追加していく
+    a += "!"
+
+    for ((i, x) in a.withIndex()) {
+        println("${i} : ${x}")
+    }
+}
