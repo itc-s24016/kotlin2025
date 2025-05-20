57ee8cf6188dd5b740803edf6eca58821c5e33f2
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-20 11:04:53 +0900

    リスト(Array_String)

---
+++ src/chap16/code16_9.kt
@@ -0,0 +1,8 @@
+package chap16
+//リスト(Array_String)
+fun main() {
+    val h = arrayOf("こんにちは", "Kotlin", "どうぞ", "よろしく", "お願いします")
+    for (x in h) {
+        println("String型の配列の値 = ${x}")
+    }
+}
