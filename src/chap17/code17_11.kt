4af8bc1bf0f820cf05fa8f0ac7ee4c3e5cba79d0
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-20 12:08:39 +0900

    mutableListの宣言の仕方まとめ

---
+++ src/chap17/code17_11.kt
@@ -0,0 +1,11 @@
+package chap17
+//mutableListの宣言の仕方まとめ
+fun main() {
+    val a: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
+    val b = mutableListOf(1, 2, 3, 4, 5)
+
+    //空のリストを用意したい...
+    val c = mutableListOf()         //これはダメ
+    val d = mutableListOf<String>() //こう記述する
+
+}
