284e67657c6272e40364e5e43b584e097ed1bc89
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-21 10:39:13 +0900

    Map_2

---
+++ src/chap19/code19_2.kt
@@ -0,0 +1,15 @@
+package chap19
+//Map_2
+fun main() {
+    val map: Map<Char, Int> = mapOf(
+        'a' to 1,
+        'b' to 2,
+        'c' to 3,
+        'z' to 26
+    )
+    println(map['a'])
+    println(map['b'])
+    println(map['c'])
+    println(map['z'])
+    println(map)
+}
