b1666bcfdfff2a52bb4657519b92c32b91fcfb37
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-22 12:41:26 +0900

    クラス

---
+++ src/chap23/code23_4.kt
@@ -0,0 +1,20 @@
+package chap23
+//クラス
+fun main() {
+    val p = Person()
+    p.name = "タケシ"
+    p.age = 5
+    p.greet()
+}
+
+class Person {
+    var name: String = ""
+    var age: Int = 0
+    fun greet(){
+        println("""
+            |こんにちは！
+            |私の名前は ${name} です
+            |年齢は ${age} 歳です
+        """.trimMargin())
+    }
+}
