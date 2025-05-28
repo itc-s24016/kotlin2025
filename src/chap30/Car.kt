c23742c1e66c0d3be3eace8a6de89962854ac01a
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-28 16:52:10 +0900

    abstract用クラス(abstract fun)

--- src/chap30/Car.kt
+++ src/chap30/Car.kt
@@ -1,10 +1,18 @@
 package chap30
-//abstract用クラス
+//abstract用クラス(abstract fun)
 abstract class Car (val color : String){
     var distance = 0.0
-    open fun drive(d: Double){
+
+    //{} を使用する = 具体的に書いたらダメ
+    abstract fun drive(d: Double)/*{
         distance += d
         println("${color} 色の車が ${d}km 走りました")
         println("その結果、マイレージ￥は ${distance} キロになりました")
-    }
+    }*/
+
+    /*注意
+    * abstract なメソッドが一つでもあるときは、クラス名に気をつけないといけない
+    * class Car
+    * open class Car
+    * これらを避けないとだめ*/
 }
