7ea6ab47ccac0e254f88e13fcd51f6aed6efea82
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-28 16:00:47 +0900

    ポリモーフィズム用クラス

---
+++ src/chap29/Ferrari.kt
@@ -0,0 +1,10 @@
+package chap29
+//ポリモーフィズム用クラス
+class Ferrari(color: String) : Car(color){
+    override fun drive(d: Double) {
+        distance += d
+        println("${color} のフェラーリが走っています！ブウォォォォーン！")
+        println("おおっ、カッコいい！！！")
+        println("マイレージは ${distance} キロになったぜい！")
+    }
+}
---
+++ src/chap29/Prius.kt
@@ -0,0 +1,11 @@
+package chap29
+//ポリモーフィズム用クラス
+class Prius(color: String) : Car(color) {
+    override fun drive(d: Double) {
+        distance += d
+        println("${color} のプリウスが走っています！スィーン！")
+        println("すごい静かですね！")
+        println("マイレージは ${distance} キロです")
+        println("トヨタの車はいっぱい走ってもなかなか故障しません！")
+    }
+}
