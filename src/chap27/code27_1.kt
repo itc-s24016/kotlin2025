195d6e4048ca5af4b9c109660d833f70a7b9ea41
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-28 14:27:25 +0900

    継承

---
+++ src/chap27/code27_1.kt
@@ -0,0 +1,20 @@
+package chap27
+//継承
+//open class = 継承可能なクラスという目印
+//open fun = オーバーライド可能なメソッド
+//override fun = オーバーライドしているメソッド
+//final override fun = オーバーライドされた先でオーバーライドされないようにする
+open class Car(){
+    var distance = 0.0
+    open fun drive(d: Double){
+        distance += d
+        println("車が ${d}km 走りました")
+        println("マイレージは ${distance}キロ になりました")
+    }
+}
+
+fun main() {
+    val car = Car()
+    car.drive(5.0)
+    car.drive(10.0)
+}
