2517d7e0227192efc838606ef886ffa82c88bb57
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-29 15:12:19 +0900

    Flyable を実装して実行

--- src/chap31/code31_7.kt
+++ src/chap31/code31_7.kt
@@ -6,9 +6,9 @@ fun main() {
     val person = FlyingPerson("タケシ", 5)
     val car = FlyingCar("赤")
     val vehicle = FlyingVehicle()
-    makeItFly(person)
-    makeItFly(car)
-    vehicle.fly()
+    makeItFly(person) //Car のメッセージ
+    makeItFly(car)    //Person のメッセージ
+    vehicle.fly()         //Flyable のメッセージ
 }
 fun makeItFly(a: Flyable){
     a.fly()
